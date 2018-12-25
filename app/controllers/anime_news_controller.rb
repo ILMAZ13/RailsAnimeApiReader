501bb963f4b61ac9550b0b32b9e5b8b8eb863ad3
ILMAZ13 <ilmaz1309@gmail.com>
2018-12-26 01:36:45 +0300

    url changed

--- app/controllers/anime_news_controller.rb
+++ app/controllers/anime_news_controller.rb
@@ -9,7 +9,7 @@ class AnimeNewsController < ApplicationController
   private
 
   def fetch_articles_from_api
-    url = "http://127.0.0.1:3001/rest"
+    url = "https://anime-api-database.herokuapp.com/rest"
     uri = URI(url)
 
     url_params = { "query" => params[:query]}
